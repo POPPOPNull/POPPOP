3550094524ecbdc98dad22fddf77fd4d4e73f84b
jdg <dgun1308@gmail.com>
2025-11-04 16:18:37 +0900

    251104 feature/admin/manager/reservation#38 [FEATURE] admin manager 팝업 스토어 별 예약 상세 조회 페이지 구현 #38

--- frontend/src/componenets/admin/adminManagerReservationList.jsx
+++ frontend/src/componenets/admin/adminManagerReservationList.jsx
@@ -1,12 +1,24 @@
 import ListContainer from "./ListContainer";
 import { selectReservationSummary } from "../../api/adminAPI";
+import { useNavigate } from "react-router-dom";
 
 function AdminManagerReservationList() {
+
+    const navigate = useNavigate();
+
+    const handleItemClick = (item) => {
+        if (item && item.popupId) {
+            navigate(`/admin/reservation/${item.popupId}`);
+        } else {
+            console.error("팝업 번호를 찾을 수 없습니다.", item);
+            alert("팝업 스토어 정보가 올바르지 않아, 페이지 이동을 할 수 없습니다.");
+        }
+    };
 
     const headers = ['팝업 이름', '진행 상태', '예약 건 수', '총 예약 인원'];
 
     const renderSummary = (summary, layoutClassName) => (
-        <div key={summary.popupName} className={`list-row ${layoutClassName}`}>
+        <div className={`list-row ${layoutClassName}`}>
             <div className="ellipsis">{summary.popupName}</div>
             <div className="ellipsis">{summary.status}</div>
             <div className="ellipsis">{summary.reservationCount} 건</div>
@@ -15,12 +27,13 @@ function AdminManagerReservationList() {
     );
 
     return(
-        <ListContainer 
-            fetchDataFunction={selectReservationSummary}    // API 호출
-            renderItem={renderSummary}               // 행 렌더링 방식
-            tableHeaders={headers}                  // 테이블 헤더
-            layoutClassName="layout-manager-reservation"
-        />
+            <ListContainer 
+                fetchDataFunction={selectReservationSummary}    // API 호출
+                renderItem={renderSummary}               // 행 렌더링 방식
+                tableHeaders={headers}                  // 테이블 헤더
+                layoutClassName="layout-manager-reservation"
+                onItemClick={handleItemClick}
+            />
     );
 }
 
