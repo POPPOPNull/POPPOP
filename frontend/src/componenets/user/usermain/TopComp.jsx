6985d18206159c20f210bb5d20d575186a39e6d8
I7E-65\i7E-63 <rjsn4433@gmail.com>
2025-11-21 17:28:22 +0900

    fix

--- frontend/src/componenets/user/usermain/TopComp.jsx
+++ frontend/src/componenets/user/usermain/TopComp.jsx
@@ -7,7 +7,8 @@ import { Autoplay, EffectCreative, Pagination } from "swiper/modules";
 import { useEffect, useState } from "react";
 import { selectAllPopupStore, selectPopupRandomly } from "../../../api/PopupStoreAPI";
 import PopupComp from "./PopupComp";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+import { logDataBySelect } from "../../../api/BehaviorAPI";
 
 
 
@@ -28,6 +29,12 @@ function TopComp (){
 
             const data2 = await selectPopupRandomly(7,length)
             setPopups(data2)
+            const array = new Array()
+            for (let i = 0 ; i < data2.length;i++){
+                array.push(data2[i].no)
+            }
+            console.log("상단",array)
+            logDataBySelect(array)
         }
         fetchData();
     },[])
