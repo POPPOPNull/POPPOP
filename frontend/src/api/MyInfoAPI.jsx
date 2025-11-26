2b68bd8cd69e2978606008d2733793ea73b886f5
DESKTOP-3OSO63K\os <ces234021@gmail.com>
2025-11-26 10:42:59 +0900

    feature/user/info 수정

--- frontend/src/api/MyInfoAPI.jsx
+++ frontend/src/api/MyInfoAPI.jsx
@@ -6,3 +6,13 @@ export function selectInfo() {
     return API.get(`${BACKEND_URL}/myinfo`)
     .then(response=>response.data);
 }
+
+export function updateEmail(email) {
+    return API.put(`${BACKEND_URL}/myinfo/email`, {email})
+    .then(response=>response.data);
+}
+
+export function updatePhone(phone) {
+    return API.put(`${BACKEND_URL}/myinfo/phone`, {phone})
+    .then(response=>response.data);
+}
